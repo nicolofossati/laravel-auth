3f3ae422502e8e229b4678673c546c6790bf792f
nicolo fossati <nicofossa01@gmail.com>
2023-05-23 09:59:18 +0200

    popolazione della tabella projects dal seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +15,19 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 15; $i++) {
+            $project = new Project;
+            $project->title = $faker->sentence(4);
+            $project->description = $faker->sentence(100);
+            $project->creation_date = $faker->dateTimeInInterval();
+            $project->completion_date = $faker->dateTimeInInterval();
+            $project->client = $faker->name();
+            $project->category = $faker->word();
+            $project->slug = Str::slug($project->title, '-');
+
+            $project->save();
+        }
     }
 }
